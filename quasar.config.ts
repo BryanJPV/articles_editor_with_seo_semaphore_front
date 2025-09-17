4e64f7f17b707071bb1ae037ed8c9aad324f1b70
BryanJPV <doppelgangerf94@gmail.com>
2025-09-17 17:29:20 -0500

    [ Editor Articulos con Semaforo de SEO FRONTEND Dev => Estructura básica de Quasar JS WIP => Se creó la configuración de Axios en src/boot/axios.ts con la api y la url principla de Admin. Se agregaron los assests de la fuente en el index.html y en assets y también los del semaforo de seo. WIP.

--- quasar.config.ts
+++ quasar.config.ts
@@ -21,7 +21,7 @@ export default defineConfig((ctx) => {
     extras: [
       // 'ionicons-v4',
       // 'mdi-v7',
-      // 'fontawesome-v6',
+      'fontawesome-v6',
       // 'eva-icons',
       // 'themify',
       // 'line-awesome',
@@ -36,6 +36,18 @@ export default defineConfig((ctx) => {
       target: {
         browser: ['es2022', 'firefox115', 'chrome115', 'safari14'],
         node: 'node20',
+      },
+
+      env: {
+        //18.220.71.160:5000
+        BACKEND_URL: 'http://localhost:5000/',
+        WEB_URL: 'https://www.activate.fm/',
+        //BACKEND_URL: 'http://18.220.71.160:5000/',
+        //WEB_URL: "http://18.220.71.160:3000/",
+        /* BACKEND_URL: 'http://212.129.4.59:5000/',
+        WEB_URL: "http://212.129.4.59:3000/", */
+        /* BACKEND_URL: 'https://app.activate.fm/',
+        WEB_URL: "https://www.activate.fm/", */
       },
 
       typescript: {
@@ -103,7 +115,25 @@ export default defineConfig((ctx) => {
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#framework
     framework: {
-      config: {},
+      config: {
+        brand: {
+          primary: '#1B77BA',
+          secondary: '#00D8FC',
+          accent: '#F3BA05',
+
+          dark: '#1d1d1d',
+          'dark-page': '#121212',
+
+          positive: '#21BA45',
+          negative: '#C10015',
+          info: '#31CCEC',
+          warning: '#F2C037',
+        },
+        notify: {
+          /* look at QUASARCONFOPTIONS from the API card (bottom of page) */
+        },
+        //dark: auto
+      },
 
       // iconSet: 'material-icons', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
@@ -116,7 +146,7 @@ export default defineConfig((ctx) => {
       // directives: [],
 
       // Quasar plugins
-      plugins: [],
+      plugins: ['Notify'],
     },
 
     // animations: 'all', // --- includes all animations
--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -1,5 +1,6 @@
 import { defineBoot } from '#q-app/wrappers';
-import axios, { type AxiosInstance } from 'axios';
+import axios, { type AxiosInstance, AxiosError } from 'axios';
+import { Notify } from 'quasar';
 
 declare module 'vue' {
   interface ComponentCustomProperties {
@@ -8,13 +9,56 @@ declare module 'vue' {
   }
 }
 
+interface ErrorResponse {
+  error: string[];
+}
+
 // Be careful when using SSR for cross-request state pollution
 // due to creating a Singleton instance here;
 // If any client changes this (global) instance, it might be a
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: 'https://api.example.com' });
+//const api = axios.create({ baseURL: 'https://api.example.com' });
+
+const interceptor = (error: AxiosError<ErrorResponse>) => {
+  //console.log(error.response.status);
+  if (error.response?.status !== undefined && error.response?.status !== null) {
+    if (error.response?.status === 403) {
+      try {
+        Notify.create({
+          type: 'negative',
+          message:
+            error.response.data?.error?.[0] ||
+            'Su sesión ha expirado, por favor inicie sesión de nuevo',
+          position: 'top',
+        });
+
+        window.location.href = '#/login';
+        return Promise.reject(error as Error);
+      } catch (error) {
+        if (error instanceof AxiosError) {
+          console.log(error);
+          console.log(error.response);
+        }
+
+        localStorage.removeItem('remember_token');
+      }
+    } else {
+      return Promise.reject(error as Error);
+    }
+  }
+};
+
+const api = axios.create({ baseURL: process.env.BACKEND_URL + 'api/' });
+const axios_aux = axios.create({ baseURL: process.env.BACKEND_URL + 'api/admin/' });
+axios.defaults.baseURL = process.env.BACKEND_URL + 'api/admin/';
+
+// Interceptors para validar los casos de error en el que el backend detecte que se le envio un token expirado, entonces retorna un error 403
+// en la gestión del error del interceptors se retorna al usuario al Login y se lo avisa mediante el notify que debe logearse de nuevo
+axios.interceptors.response.use((response) => response, interceptor);
+api.interceptors.response.use((response) => response, interceptor);
+axios_aux.interceptors.response.use((response) => response, interceptor);
 
 export default defineBoot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
@@ -26,6 +70,7 @@ export default defineBoot(({ app }) => {
   app.config.globalProperties.$api = api;
   // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
   //       so you can easily perform requests against your app's API
+  app.config.globalProperties.$axios_aux = axios_aux;
 });
 
-export { api };
+export { api, axios_aux };
